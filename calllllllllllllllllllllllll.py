a9fe2363297234d24c44df9a6502559c64ee75ae
Leonelta <ketchakaren@gmail.com>
2021-04-08 10:18:02 +0200

    create the display screen
    to be able to display the output or the different operation

--- calllllllllllllllllllllllll.py
+++ calllllllllllllllllllllllll.py
@@ -8,6 +8,6 @@ text_input = StringVar()
 txtDisplay = Entry(cal,font=('arail', 20, 'bold') ,textvariable=text_input , bd=30, insertwidth=4,
                               bg="powder blue", justify='right').grid(columnspan=4)
 
-btn7=Button(cal, padx=16, bd= 8 ,fg = "black", font=('arail', 20, 'bold'),
-            text ="7").grid(row = 1,column = 0)
+btn7 = Button(cal, padx = 16, bd = 0, fg = "black", font=('arail', 20, 'bold'),
+              text= "7").grid(row = 1, column = 0)
 cal.mainloop()
