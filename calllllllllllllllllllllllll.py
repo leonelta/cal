055b6eb4e788fa7fc71fe9b12ec64b7df7927203
Leonelta <ketchakaren@gmail.com>
2021-04-08 22:03:26 +0200

    defining a clearDisplay functiin
    which is only used to  control the btn clear

--- calllllllllllllllllllllllll.py
+++ calllllllllllllllllllllllll.py
@@ -5,6 +5,11 @@ def btnClick(numbers):
     operator = operator + str(numbers)
     text_Input.set(operator)
 
+def btnClearDisplay():
+    global operator
+    operator = ""
+    text_Input.set("")
+
 cal = Tk()
 cal.title("calculator")
 operator = ""
@@ -17,49 +22,49 @@ btn7 = Button(cal, padx = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
               text= "7", bg="powder blue" ,  command=lambda:btnClick(7)).grid(row = 1, column = 0)
 
 btn8 = Button(cal, padx = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
-              text= "8", bg="powder blue").grid(row = 1, column = 1)
+              text= "8", bg="powder blue", command=lambda:btnClick(8)).grid(row = 1, column = 1)
 
 btn9 = Button(cal, padx = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
-              text= "9", bg="powder blue").grid(row = 1, column = 2)
-
+              text= "9", bg="powder blue", command=lambda:btnClick(9)).grid(row = 1, column = 2)
+              
 Addition =  Button(cal, padx = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
-              text= "+", bg="powder blue").grid(row = 1, column = 3)
+              text= "+", bg="powder blue", command=lambda:btnClick("+")).grid(row = 1, column = 3)
 
 btn4 = Button(cal, padx = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
-              text= "4", bg="powder blue").grid(row = 2, column = 0)
+              text= "4", bg="powder blue", command=lambda:btnClick(4)).grid(row = 2, column = 0)
 
 btn5 = Button(cal, padx = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
-              text= "5", bg="powder blue").grid(row = 2, column = 1)
+              text= "5", bg="powder blue", command=lambda:btnClick(5)).grid(row = 2, column = 1)
 
 btn6 = Button(cal, padx = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
-              text= "6", bg="powder blue").grid(row = 2, column = 2)
+              text= "6", bg="powder blue", command=lambda:btnClick(6)).grid(row = 2, column = 2)
 
 subtraction =  Button(cal, padx = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
-              text= "-", bg="powder blue").grid(row = 2, column = 3)
+              text= "-", bg="powder blue", command=lambda:btnClick("-")).grid(row = 2, column = 3)
 
 btn1 = Button(cal, padx = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
-              text= "1", bg="powder blue").grid(row = 3, column = 0)
+              text= "1", bg="powder blue", command=lambda:btnClick(1)).grid(row = 3, column = 0)
 
 btn2 = Button(cal, padx = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
-              text= "2", bg="powder blue").grid(row = 3, column = 1)
+              text= "2", bg="powder blue" , command=lambda:btnClick(2)).grid(row = 3, column = 1)
 
 btn3 = Button(cal, padx = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
-              text= "3", bg="powder blue").grid(row = 3, column = 2)
+              text= "3", bg="powder blue" , command=lambda:btnClick(3)).grid(row = 3, column = 2)
 
 multiply =  Button(cal, padx = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
-              text= "*", bg="powder blue").grid(row = 3, column = 3)
+              text= "*", bg="powder blue" ,command=lambda:btnClick("*")).grid(row = 3, column = 3)
 
 btn0 = Button(cal, padx = 16,pady = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
-              text= "0", bg="powder blue").grid(row = 4, column = 0)
+              text= "0", bg="powder blue", command=lambda:btnClick(0)).grid(row = 4, column = 0)
 
 btnClear = Button(cal, padx = 16, pady = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
-              text= "C", bg="powder blue").grid(row = 4, column = 1)
+              text= "C", bg="powder blue", command= btnClearDisplay).grid(row = 4, column = 1)
 
 btnEqual = Button(cal, padx = 16,pady = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
-              text= "=", bg="powder blue").grid(row = 4, column = 2)
+              text= "=", bg="powder blue" ).grid(row = 4, column = 2)
 
 Division =  Button(cal, padx = 16,pady = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
-             text= "/", bg="powder blue").grid(row = 4, column = 3)
+             text= "/", bg="powder blue", command=lambda:btnClick("/")).grid(row = 4, column = 3)
 
 
 
