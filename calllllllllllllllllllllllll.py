f873ce63d83dcd1106799536e1017c6b1f65b524
Leonelta <ketchakaren@gmail.com>
2021-04-08 11:00:25 +0200

    making button 7,8,9
    adjusting to be on row 1 and have different column

--- calllllllllllllllllllllllll.py
+++ calllllllllllllllllllllllll.py
@@ -8,6 +8,28 @@ text_input = StringVar()
 txtDisplay = Entry(cal,font=('arail', 20, 'bold') ,textvariable=text_input , bd=30, insertwidth=4,
                               bg="powder blue", justify='right').grid(columnspan=4)
 
-btn7 = Button(cal, padx = 16, bd = 0, fg = "black", font=('arail', 20, 'bold'),
-              text= "7").grid(row = 1, column = 0)
+ubtn7 = Button(cal, padx = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
+              text= "7", bg="powder blue").grid(row = 1, column = 0)
+
+btn8 = Button(cal, padx = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
+              text= "8", bg="powder blue").grid(row = 1, column = 1)
+
+btn9 = Button(cal, padx = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
+              text= "9", bg="powder blue").grid(row = 1, column = 2)
+
+Addition =  Button(cal, padx = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
+              text= "+", bg="powder blue").grid(row = 1, column = 3)
+
+btn7 = Button(cal, padx = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
+              text= "7", bg="powder blue").grid(row = 1, column = 0)
+
+btn8 = Button(cal, padx = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
+              text= "8", bg="powder blue").grid(row = 1, column = 1)
+
+btn9 = Button(cal, padx = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
+              text= "9", bg="powder blue").grid(row = 1, column = 2)
+
+Addition =  Button(cal, padx = 16, bd = 8, fg = "black", font=('arail', 20, 'bold'),
+              text= "+", bg="powder blue").grid(row = 1, column = 3)
+
 cal.mainloop()
